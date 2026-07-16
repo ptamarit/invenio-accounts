a4fde4b1c49ef585451934881d464c910e3364f9
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-16 16:53:48 +0200

    release: v9.0.1

--- invenio_accounts/__init__.py
+++ invenio_accounts/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2015-2026 CERN.
 # SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
 # SPDX-FileCopyrightText: 2025 KTH Royal Institute of Technology.
+# SPDX-FileCopyrightText: 2026 TU Wien.
 # SPDX-License-Identifier: MIT
 
 """Invenio user management and authentication.
@@ -51,7 +52,7 @@ except AttributeError:
 from .ext import InvenioAccounts, InvenioAccountsREST, InvenioAccountsUI
 from .proxies import current_accounts
 
-__version__ = "9.0.0"
+__version__ = "9.0.1"
 
 __all__ = (
     "__version__",
